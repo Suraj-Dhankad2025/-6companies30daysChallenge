04a31ddd6c817ed24bcbe6ea528ac2fa238176d4
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-15 20:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (57.04%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string>a;
+    void find(int i, string temp, string &s, set<string>&st, vector<string>&ans){
+        if(i>=s.size()){
+            temp.pop_back();
+            ans.push_back(temp);
+            return ;
+        }
+        for(int j=i; j<s.size(); j++){
+            if(st.find(s.substr(i, j-i+1))!=st.end()){
+                find(j+1, temp+s.substr(i, j-i+1)+" ", s, st, ans);
+            }
+        }
+        
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string>ans;
+        set<string>st;
+        for(auto i:wordDict){
+            st.insert(i);
+        }
+        find(0, "", s, st, ans);
+        return ans;
+    }
+};
