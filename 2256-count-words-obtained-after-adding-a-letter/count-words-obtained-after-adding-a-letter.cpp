ea83211c899fc2752c3d991b205e70c1c1ddb469
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-23 23:08:54 +0530

    Time: 544 ms (32.00%) | Memory: 120.7 MB (29.14%) - LeetSync

---
+++ 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
+        set<string>s;
+        for(auto i:startWords){
+            string temp = i;
+            sort(temp.begin(), temp.end());
+            s.insert(temp);
+        }
+        int ans=0;
+        for(auto i:targetWords){
+            string temp = i;
+            sort(temp.begin(), temp.end());
+            for(int ind=0; ind<temp.size(); ind++){
+                string t = temp.substr(0,ind) + temp.substr(ind+1);
+                if(s.find(t)!=s.end()){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
