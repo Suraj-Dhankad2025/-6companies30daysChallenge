5f31e783f2ab8817322a6457b07f65604e6eed2f
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-27 15:23:47 +0530

    Time: 26 ms (59.48%) | Memory: 19 MB (18.50%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.cpp
+++ 218-the-skyline-problem/the-skyline-problem.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    static bool cmp(pair<int, int> a, pair<int, int> b) {
-        if (a.first == b.first) {
-            return a.second < b.second;
-        }
-        return a.first < b.first;
-    }
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
         vector<pair<int, int>>v;
         for(auto i:buildings){
             v.push_back({i[0], -i[2]});
             v.push_back({i[1], i[2]});
         }
-        sort(v.begin(), v.end(), cmp);
+        sort(v.begin(), v.end());
         vector<vector<int>>ans;
         multiset<int>s;
         int curH = 0;
